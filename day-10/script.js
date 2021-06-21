7408008f846268054eb5964d4d84a8a998947a4f
Vinit <vs361017@gmail.com>
2021-06-21 15:04:39 +0530

    updated day10

--- day-10/script.js
+++ day-10/script.js
@@ -1,5 +1,6 @@
-const backFace = document.querySelectorAll(".back-face");
 const cards = document.querySelectorAll(".card");
+const container = document.querySelector(".container");
+const resetBtn = document.querySelector(".btn");
 
 let cardsSelected = [];
 let cardsId = [];
@@ -11,6 +12,9 @@ function flipCard() {
   cardsId.push(this.getAttribute("data-id"));
   // console.log(cardsSelected);
   // console.log(cardsId);
+  if (cardsSelected.length > 2) {
+    this.classList.remove("flip");
+  }
   if (cardsSelected.length === 2) {
     setTimeout(checkForMatch, 500);
   }
@@ -19,23 +23,47 @@ function flipCard() {
 function checkForMatch() {
   let firstCard = cards[cardsId[0] - 1];
   let secondCard = cards[cardsId[1] - 1];
+  console.log(cardsWon.length);
 
-  if (cardsId[0] == cardsId[1]) {
+  if (cardsId[0] === cardsId[1]) {
     // console.log("clicked same img");
     cardsId.pop();
     cardsSelected.pop();
   } else if (cardsSelected[0] === cardsSelected[1]) {
-    // console.log("WON");
+    // console.log("Matched");
     firstCard.classList.add("hide");
     secondCard.classList.add("hide");
     cardsWon.push(cardsSelected);
+    if (cardsWon.length === cards.length / 2) {
+      container.innerHTML = `
+        <h1>You Won!!</h1>
+      `;
+      resetBtn.innerHTML = "Play Again?";
+      container.classList.add("won");
+    }
   } else {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
+    removeFlip(firstCard, secondCard);
   }
 
   cardsSelected = [];
   cardsId = [];
 }
+
+function removeFlip(card1, card2) {
+  card1.classList.remove("flip");
+  card2.classList.remove("flip");
+}
+
+(function shuffleCards() {
+  cards.forEach((card) => {
+    let randomOrder = Math.floor(Math.random() * cards.length);
+    card.style.order = randomOrder;
+  });
+})();
+
+resetBtn.addEventListener("click", () => {
+  cards.forEach((card) => card.classList.add("reset"));
+  setTimeout(() => location.reload(), 500);
+});
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
