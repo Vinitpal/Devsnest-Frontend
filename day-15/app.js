51b970740ddc52de5296c33021565b89556c0c5f
Vinit <vs361017@gmail.com>
2021-07-05 22:06:36 +0530

    day15

---
+++ day-15/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const e = React.createElement;
+
+class Sidd extends React.Component {
+  render() {
+    return "h2", {}, "hellooooooooo";
+  }
+}
+
+ReactDOM.render(e(Sidd), document.getElementById("root"));
