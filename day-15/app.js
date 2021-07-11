813b167eac044562516a3478a0575e9c92c5abec
Vinit <vs361017@gmail.com>
2021-07-11 20:21:06 +0530

    updated js

--- day-15/app.js
+++ day-15/app.js
@@ -3,8 +3,21 @@
 const e = React.createElement;
 
 class Sidd extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
+
   render() {
-    return "h2", {}, "hellooooooooo";
+    if (this.state.clicked) {
+      return "SAIKYOO";
+    }
+
+    return e(
+      "button",
+      { onClick: () => this.setState({ clicked: true }) },
+      "click me"
+    );
   }
 }
 
